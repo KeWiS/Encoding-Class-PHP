a0a958fbbb4f3d8c71b41e9593bb4d91fc827491
Michał Siwek <mjzsiwek@gmail.com>
2020-09-09 01:29:52 +0200

    Fixed not working UTF-8 detection

--- File input version/Encoding.php
+++ File input version/Encoding.php
@@ -5,7 +5,7 @@
         //toUTF8 string converting method
         static function toUTF8($string){
             //Detecting ISO-8859-2 coding (in specific order)
-            $coding = mb_detect_encoding($string, "ISO-8859-2,UTF-8");
+            $coding = mb_detect_encoding($string, "UTF-8,ISO-8859-2");
 
             //Test lines for checking if everything runs correctly, used for testings
             //Coding of string before encoding
@@ -43,6 +43,6 @@
         //Creating variable with return value after calling encoding method toUTF8()
         $result = $encode->toUTF8($string);
         //Printing results in disabled text field
-        echo "<h3>Encoded string to UTF-8:</h3><br><input type = text value = $result disabled>";
+        echo "<h3>Encoded string to UTF-8:</h3><br><textarea disabled>$result</textarea>";
     }
 ?>
