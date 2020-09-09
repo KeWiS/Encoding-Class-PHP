9734018840906101c5d5ebfa0e39c9530a7ec547
Michał Siwek <mjzsiwek@gmail.com>
2020-09-09 07:53:24 +0200

    Added detecting Windows 1250 encoding, fixed encoding for every 3 input encodings

--- File input version/Encoding.php
+++ File input version/Encoding.php
@@ -4,10 +4,20 @@
         
         //toUTF8 string converting method
         static function toUTF8($string){
-            //Detecting ISO-8859-2 coding (in specific order)
-            $coding = mb_detect_encoding($string, "UTF-8,ISO-8859-2");
+            //Detecting possibly "false" encoding
+            $coding = mb_detect_encoding($string, "UTF-8, ISO-8859-2");
+            //If detected encoding is ISO-8859-2 it might be false, if statement checks it
+            if($coding == "ISO-8859-2"){
+                //Forcing to encode from ISO-8859-2 to Windows-1250, then from Windows-1250 to UTF-8
+                //IGNORE after output encoding is for getting rid of bug notice, in this case we care only about results of encoding
+                $coding_check = iconv("ISO-8859-2", "Windows-1250//IGNORE", $string);
+                $coding_check = iconv("Windows-1250", "UTF-8//IGNORE", $coding_check);
+                //Doing normal encoding from ISO-8859-2 to UTF-8
+                $check_verification = iconv("ISO-8859-2", "UTF-8", $string);
+                //Checking if both conversions are correct, if they are not - coding is Windows-1250, if the are - keep coding ISO-8859-2
+                if($check_verification != $coding_check)    $coding = "Windows-1250";
+            }
 
-            //Test lines for checking if everything runs correctly, used for testings
             //Coding of string before encoding
             echo "Coding of string before encoding: $coding<br>";
             //String before encoding
